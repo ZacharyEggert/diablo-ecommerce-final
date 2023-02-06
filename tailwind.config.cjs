715b8aca778f81e9a43c6e2fb2fa6c6fa7a036bc
Zachary Eggert <Eggert.Zachary@gmail.com>
2023-02-06 13:33:34 -0800

    tailwind extensions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/container-queries"),
+  ],
 };
